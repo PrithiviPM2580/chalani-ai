a528980b958cc6b802a8fce436e838d7d01c7ada
PrithiviPM2580 <prithivipemimagar>
2025-09-30 00:39:57 +0545

    fix: update parameter name in signUpController for consistency

--- backend/src/controllers/auth/sign-up.ts
+++ backend/src/controllers/auth/sign-up.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
 
-const signUpContoller = async (req: Request, res: Response): Promise<void> => {
+const signUpContoller = async (_req: Request, res: Response): Promise<void> => {
   res.send('sign up controller');
 };
 
